0732be48e8ea5e3672ef211cc4929d28deb68ab6
a-llie <alexstefanakis@cmail.carleton.ca>
2024-03-16 15:40:35 -0400

    Error handling

--- functions.py
+++ functions.py
@@ -10,32 +10,46 @@ Else:  Quit\n"""
 # Retrieves and displays all records from the students table.
 def getAllStudents():
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM students")
-        print(cur.fetchall())
-
+        try:
+            cur.execute("SELECT * FROM students")
+            print(cur.fetchall())
+        except:
+            print("\nERR: An error occurred while retrieving the students.")
+            conn.rollback()
 
 # addStudent(first_name, last_name, email, enrollment_date)
 # Inserts a new student record into the students table.
 def addStudent(first_name, last_name, email, enrollment_date):
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s)", (first_name, last_name, email, enrollment_date))
-        conn.commit()
+        try:
+            cur.execute("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s)", (first_name, last_name, email, enrollment_date))
+            conn.commit()
+        except:
+            print("\nERR: An error occurred while adding the student.")
+            conn.rollback()
 
 # updateStudentEmail(student_id, new_email)
 # Updates the email address for a student with the specified student_id.
 def updateStudentEmail(student_id, new_email):
     with conn.cursor() as cur:
-        cur.execute("UPDATE students SET email = %s WHERE student_id = %s", (new_email, student_id))
-        conn.commit()   
+        try:
+            cur.execute("UPDATE students SET email = %s WHERE student_id = %s", (new_email, student_id))
+            conn.commit()
+        except: 
+            print("\nERR: An error occurred while updating the student's email.")
+            conn.rollback()   
 
 
 # deleteStudent(student_id)
 # Deletes the record of the student with the specified student_id.
 def deleteStudent(student_id):
     with conn.cursor() as cur:
-        cur.execute("DELETE FROM students WHERE student_id = %s", (student_id))
-        conn.commit()   
-
+        try:
+            cur.execute("DELETE FROM students WHERE student_id = %s", (student_id))
+            conn.commit()   
+        except:
+            print("\nERR: An error occurred while deleting the student.")
+            conn.rollback()
 
 # getNewStudentDetails()
 # Prompts the user to enter the details of a new student.
@@ -85,10 +99,14 @@ if __name__ == "__main__":
     user =  input("Enter username\n>>")
     pw = input("Enter password\n>>")
 
-    with psycopg2.connect(f'dbname={db} user={user} password={pw}') as conn:
-        print('Choose one of the following options: ')
-        while True:
-            prompt()
+    try:
+        with psycopg2.connect(f'dbname={db} user={user} password={pw}') as conn:
+            print('Choose one of the following options: ')
+            while True:
+                prompt()
+    except psycopg2.OperationalError as e:
+        print("\nERR: An error occurred while connecting to the database.")
+        exit()
 
 
 
